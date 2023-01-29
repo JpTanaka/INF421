134a90d5e7b6ef8797c2f7139fe2dc02e03901e6
JpTanaka <jpao.com@gmail.com>
2023-01-29 19:50:50 +0100

    read_file now read queries

--- main.py
+++ main.py
@@ -2,14 +2,21 @@ from graph import Graph
 
 def read_file(file_name):
     with open(file_name, "r") as file:
-        first_line = file.readline()
-        n, m = map(int, first_line.split())
+        graph_line = file.readline()
+        n, m = map(int, graph_line.split())
         edges = []
-        for i in range(m):
+        for _ in range(m):
             edges.append([int(x) for x in file.readline().split()])
 
-        return [n, edges]
+
+        queries = []
+        n_queries = int(file.readline())
+        for _ in range(n_queries):
+            queries.append([int(x) for x in file.readline().split()])
+        return [n, m, edges, queries]
 
 
 if __name__=="__main__":
-    print(read_file("/home/nakatinha/repos/INF421/tests/tests/itineraries.0.in"))
+    test_0 = read_file("/home/nakatinha/repos/INF421/tests/tests/itineraries.0.in")
+
+    
